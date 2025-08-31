f123f2e9de5ca4d89ec605ca4e15b2d8d793d6b4
Deimz <s2010708@usls.edu.ph>
2025-08-31 16:21:07 +0800

    message

--- client/src/components/GalleryGrid.js
+++ client/src/components/GalleryGrid.js
@@ -1,48 +1,45 @@
 import React from 'react'
 
 function GalleryGrid() {
-  return (
-    <div className="container">
-      <div className="gallery-header">
-        <h2 className="gallery-title">GALLERY</h2>
-         <p className= "gallery-description">This is the gallery showcase</p>
-      </div>
+  return (<>
+    <div class="gallery-container">
+        <div class="gallery-header">
+            <h1>Image Gallery</h1>
+            <p>Explore our curated collection of stunning visuals and moments captured in time</p>
+        </div>
+        <div class="gallery-grid" id="galleryGrid">
+        </div>
 
-      <div className="gallery-grid" id="galleryGrid">
-        <div className="gallery-item">
-        <img src= "" alt=""/>
-        <div class="gallery-overlay">
-          <div class="overlay-title">Social Media</div>
-          <div class="overlay-description">Instagram interface design</div>
-           </div>
-         </div>
-
-         <div className="gallery-item">
-        <img src= "" alt=""/>
-        <div class="gallery-overlay">
-          <div class="overlay-title">Social Media</div>
-          <div class="overlay-description">Instagram interface design</div>
-           </div>
-         </div>
-
-         <div className="gallery-item">
-        <img src= "" alt=""/>
-        <div class="gallery-overlay">
-          <div class="overlay-title">Social Media</div>
-          <div class="overlay-description">Instagram interface design</div>
-           </div>
-         </div>
-
-         <div className="gallery-item">
-        <img src= "" alt=""/>
-        <div class="gallery-overlay">
-          <div class="overlay-title">Social Media</div>
-          <div class="overlay-description">Instagram interface design</div>
-           </div>
-         </div>
-         
-      </div>
+        <div class="load-more">
+            <button class="load-more-btn" id="loadMoreBtn">Load More</button>
+        </div>
     </div>
+
+    <div class="lightbox" id="lightbox">
+        <div class="lightbox-content">
+            <button class="lightbox-close" id="lightboxClose">
+                <svg fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+            <button class="lightbox-nav lightbox-prev" id="lightboxPrev">
+                <svg fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+            <button class="lightbox-nav lightbox-next" id="lightboxNext">
+                <svg fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+            <img class="lightbox-image" id="lightboxImage" src="" alt=""/>
+            <div class="lightbox-info" id="lightboxInfo">
+                <h3 id="lightboxTitle"></h3>
+                <p id="lightboxDescription"></p>
+            </div>
+        </div>
+    </div>
+   </>
   )
 }
 
